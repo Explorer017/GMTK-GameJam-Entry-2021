b7c04eebf740eaa366851cfc40fa7c5b537fa561
JakeyGilly <jakegillman3@gmail.com>
2021-06-13 00:45:12 +0100

    x

--- Assets/Scripts/CamFollow.cs
+++ Assets/Scripts/CamFollow.cs
@@ -1,16 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CamFollow : MonoBehaviour
 {
-
     public Transform playerTransform;
-    private void LateUpdate()
-    {
-        Vector3 temp = transform.position;
-        temp.x = playerTransform.position.x;
-        transform.position = temp;
-    }
-
+    public float upperBound;
+    public float lowerBound;
+    public float leftBound;
+    public float rightBound;
+    private void LateUpdate() {transform.position = new Vector3(Mathf.Clamp(playerTransform.position.x, leftBound, rightBound),Mathf.Clamp(playerTransform.position.y, lowerBound, upperBound), transform.position.z);}
 }
