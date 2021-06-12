63f7ba06c5805d699f1a4121f1c8dccda8e9da27
JakeyGilly <jakegillman3@gmail.com>
2021-06-12 17:08:35 +0100

    beans

--- Assets/Scripts/Blindness.cs
+++ Assets/Scripts/Blindness.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Experimental.Rendering.Universal;
 public class Blindness : MonoBehaviour
 {
+    public Light2D light;
+    public Color color;
     // Start is called before the first frame update
     void Start()
     {
-        
+        light = GetComponent<Light2D>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        light.pointLightOuterRadius += (float)0.01;
     }
 }
