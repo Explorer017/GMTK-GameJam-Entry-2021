84d23eda4ab6bbc6b03d96c9445e2cbcc3020128
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2021-06-12 12:30:31 +0100

    atempting to do jumpy jump

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpspeed;
+    private Rigidbody2D rb;
+    public Vector2 movement;
+    public bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        Camera.main.transform.position = new Vector3(this.transform.position.x, 0.5f, -10f);
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Entered");
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Exited");
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
+
