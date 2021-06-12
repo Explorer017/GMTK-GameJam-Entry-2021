eae724a2c50c8d24eeedc24b1612899a70fa47f7
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2021-06-12 13:34:53 +0100

    changed things and stuff

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+
+    public Transform playerTransform;
+    private void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+        temp.x = playerTransform.position.x;
+        transform.position = temp;
+    }
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,31 +23,11 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
-    }
-
-    void Update(){
         if(isGrounded){
             extraJumps = extraJumpsValue;
         }
-        if((Input.GetAxis("Jump") == 1)  && (extraJumps > 0)){
-            if (jumpHeld == false)
-            {
-                rb.velocity = Vector2.up * jumpspeed;
-                extraJumps--;
-            }
-            jumpHeld = true;
-        } else if ((Input.GetAxis("Jump") == 1) && (extraJumps == 0) && isGrounded) {
-            if (jumpHeld == false)
-            {
-                rb.velocity = Vector2.up * jumpspeed;
-            }
-            jumpHeld = true;
-        }
-        else
-        {
-            jumpHeld = false;
-        }
+        if((Input.GetAxis("Jump") == 1)  && (extraJumps > 0)){if (jumpHeld == false){rb.velocity = Vector2.up * jumpspeed;extraJumps--;}jumpHeld = true;} else if ((Input.GetAxis("Jump") == 1) && (extraJumps == 0) && isGrounded) {if (jumpHeld == false) {rb.velocity = Vector2.up * jumpspeed;} jumpHeld = true;} else {jumpHeld = false;}
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
     }
 }
 
