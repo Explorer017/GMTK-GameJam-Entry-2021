9edce4e8ea27664120aa4d6fcdfff00a716d037e
JakeyGilly <jakegillman3@gmail.com>
2021-06-13 16:43:30 +0100

    fixed buttons

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -5,14 +5,17 @@ public class EndScreen : MonoBehaviour
 {
     public void playAgain()
     {
+        Debug.Log("PLEASE HELP ME1");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void nextLevel()
     {
+        Debug.Log("PLEASE HELP ME2");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void toMenu()
     {
+        Debug.Log("PLEASE HELP ME3");
         SceneManager.LoadScene(0);
     }
 }
